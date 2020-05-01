084ffc8d2f65dd64714e6e0669c139d1b1b8060f
David Humphries <d.a.humphries@gmail.com>
2020-05-02 00:30:58 +1200

    basic dbf generation from sheet

--- fromExcel.py
+++ fromExcel.py
@@ -1,16 +1,21 @@
 from openpyxl import load_workbook
 from openpyxl import utils
+import dbf
 
 file = "IO_INSTRUMENT_LIST.xlsx"
 sheet = "DEFAULTS"
+divider = "____________________________________________________________________"
 
 
 def main(fileName, sheetName):
-    data = loadData(fileName, sheetName)
+    data = getData(fileName, sheetName)
     createDBF(data)
 
 
-def loadData(fileName, sheetName):
+def getData(fileName, sheetName):
+    print("")
+    print(divider)
+    print("Getting data from sheet")
 
     wb = load_workbook(filename=file, read_only=False,
                        keep_vba=False, data_only=True, keep_links=False)
@@ -19,83 +24,92 @@ def loadData(fileName, sheetName):
     data = {
         "file": fileName,
         "sheet": sheetName,
-        "prototypes": [],
-        "fields": [],
+        "protoNames": [],
+        "fieldNames": [],
+        "entries": [],
+        "variables": []
+
     }
-    for index in range(ws.max_row):
-        cell = ws["A"+str(index+1)]
-        if cell.value != None:
-            data["fields"].append(cell.value)
-            # print()
-    for index in range(ws.max_column):
-        cell = ws[utils.get_column_letter(index+1)+"2"]
-        if cell.value != None:
-            data["prototypes"].append(cell.value)
 
-        # print()
-        # print(index)
+    def getFieldNames():
+        for rowIndex in range(3, 12):
+            cell = ws["A"+str(rowIndex)]
+            if cell.value != None:
+                data["fieldNames"].append(cell.value)
+        print("Got FieldNames")
 
+    def getProtoNames():
+        for colIndex in range(2, 12):
+            cell = ws[utils.get_column_letter(colIndex)+"2"]
+            if cell.value != None:
+                data["protoNames"].append(cell.value)
+        print("Got ProtoNames")
+
+    def getEntries():
+        for colIndex in range(2, 12):
+            rows = []
+            for rowIndex in range(3, 12):
+                cell = ws[utils.get_column_letter(colIndex)+str(rowIndex)]
+                rows.append(cell.value)
+            data["entries"].append(rows)
+        print("Got Entries")
+
+    def getVariables():
+        for colIndex in range(2, 12):
+            rows = []
+            for rowIndex in range(3, 12):
+                cell = ws[utils.get_column_letter(colIndex)+str(rowIndex)]
+                if iterable(cell.value):
+                    if "##" in cell.value:
+                        rows.append(cell.value)
+                    else:
+                        rows.append(None)
+                else:
+                    rows.append(None)
+            data["variables"].append(rows)
+        print("Got Variables")
+
+    getFieldNames()
+    getProtoNames()
+    getEntries()
+    getVariables()
+    print("")
+    print("Got All Data")
+    print(divider)
     return data
 
 
 def createDBF(data):
-    print(data)
+    print("")
+    print(divider)
+    print("Generating " + "prototype" + ".dbf")
+    for protoName in data["protoNames"]:
+        protoIndex = data["protoNames"].index(protoName)
+        specs = ""
+        for fieldName in data["fieldNames"]:
+            fieldIndex = data["fieldNames"].index(fieldName)
+            if data["entries"][protoIndex][fieldIndex] != None:
+                specs += fieldName + " C(16); "
+            if specs != "":
+                dbfTable = dbf.Table(
+                    filename=protoName+".dbf",
+                    field_specs=specs,
+                )
+            dbfTable.open(mode=dbf.READ_WRITE)
+
+    # print(data["entries"])
+    # print(data["fieldNames"])
+    # print(data["variables"])
     # print(data["prototypes"])
 
 
+def iterable(obj):
+    try:
+        iter(obj)
+    except Exception:
+        return False
+    else:
+        return True
+
+
 main(file, sheet)
-
-# def get_all_tables(filename):
-#     """ Get all tables from a given workbook. Returns a dictionary of tables.
-#         Requires a filename, which includes the file path and filename. """
-
-#     # Load the workbook, from the filename
-#     wb = load_workbook(filename=file, read_only=False,
-#                        keep_vba=False, data_only=True, keep_links=False)
-
-#     # Initialize the dictionary of tables
-#     tables_dict = {}
-
-#     # Go through each worksheet in the workbook
-#     for ws_name in wb.sheetnames:
-#         print("")
-#         print(f"worksheet name: {ws_name}")
-#         ws = wb[ws_name]
-#         # print(f"tables in worksheet: {len(ws._tables)}")
-
-#         # Get each table in the worksheet
-#         for tbl in ws._tables:
-#             print(f"table name: {tbl.name}")
-#             # First, add some info about the table to the dictionary
-#             tables_dict[tbl.name] = {
-#                 'table_name': tbl.name,
-#                 'worksheet': ws_name,
-#                 'num_cols': len(tbl.tableColumns),
-#                 'table_range': tbl.ref}
-
-#             # Grab the 'data' from the table
-#             data = ws[tbl.ref]
-
-#             # Now convert the table 'data' to a Pandas DataFrame
-#             # First get a list of all rows, including the first header row
-#             rows_list = []
-
-#             for row in data:
-#                 # Get a list of all columns in each row
-#                 cols = []
-#                 for col in row:
-#                     cols.append(col.value)
-#                 rows_list.append(cols)
-
-#             # Create a pandas dataframe from the rows_list.
-#             # The first row is the column names
-#             df = data
-
-#             # Add the dataframe to the dictionary of tables
-#             tables_dict[tbl.name]['dataframe'] = df
-
-#     return tables_dict
-
-
-# File location:
-# Run the function to return a dictionary of all tables in the Excel workbook
